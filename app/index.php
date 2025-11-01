5ae74026b717b53683924f758acdd115d0c117c5
limlinli <galyanikif24@gmail.com>
2025-11-01 04:54:21 +0300

    Update index.php

--- app/index.php
+++ app/index.php
@@ -11,7 +11,7 @@
     <title>Hello, world!</title>
   </head>
   <body>
-  <p><a href="tovari.php">Список товаров</a></p>
+  <p><a href="tovari.php">Список </a></p>
   <p><a href="cart.php">Корзина</a></p>
   <p><a href="checkout.php">Checkout</a></p>
   <div class="container">
@@ -181,4 +181,5 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
+
 </html>
